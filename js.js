a53484fb2b62ed7179abccb6e86570eea42085e0
Bambi02 <rezigren@gmail.com>
2020-03-06 11:45:36 +0100

    prepisany JS

--- js.js
+++ js.js
@@ -1,27 +1,27 @@
 const pocetObrazkov = 8;
 const array = [];
-const button = document.querySelector(".zmena");
+const button = document.querySelector(`.zmena`);
+var pozicia1 = 1;
+var pozicia2 = 11;
 
 for (let i=1; i<=pocetObrazkov; i++){
     array.push(i);
 }
 
-function zmena(){    
+generateImagesFront = (img) => document.getElementById(`img${pozicia1}`).src=`img/${img}.webp`;
+generateImagesBack = (img2) => document.getElementById(`img${pozicia2}`).src=`img/${img2}.webp`;
+
+zmena =() => {    
     array.sort(() => Math.random() - 0.5);
-
-        document.getElementById("img1").src="img/"+array[0]+".webp";
-        document.getElementById("img2").src="img/"+array[1]+".webp";
-        document.getElementById("img3").src="img/"+array[2]+".webp";
-        document.getElementById("img4").src="img/"+array[3]+".webp";
-        document.getElementById("img5").src="img/"+array[4]+".webp";
-        document.getElementById("img6").src="img/"+array[5]+".webp";
-
-        document.getElementById("img11").src="img/"+array[1]+".webp";
-        document.getElementById("img12").src="img/"+array[2]+".webp";
-        document.getElementById("img13").src="img/"+array[3]+".webp";
-        document.getElementById("img14").src="img/"+array[4]+".webp";
-        document.getElementById("img15").src="img/"+array[5]+".webp";
-        document.getElementById("img16").src="img/"+array[6]+".webp";
+    for (let i = 0; i< 6; i++){
+        generateImagesFront(array[i]);
+        generateImagesBack(array[i+1]);
+        pozicia1++
+        pozicia2++
+    }
+    pozicia1=1;
+    pozicia2=11;
 }
 
 button.addEventListener("click", zmena)
+
